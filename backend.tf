24d4987aa95ef23e70e77fdd2d3fe51162e995da
Anonymous_14 <yogirajgite123@gmail.com>
2025-04-09 13:32:21 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -3,6 +3,6 @@ terraform {
     bucket = "sample-bukcet-batch-27"
     key = "main"
     region = "us-east-1"
-    dynamodb_table = "my-dynamodb-table"
+    dynamodb_table = "my-dynamodb-table-1"
   }
 }
